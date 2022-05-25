e69db8267267971e86a4317861c34c8f996d2cb7
Paulo Silva <silva.paulohr@gmail.com>
2022-05-25 18:48:16 -0300

    feat(server): add init method for app

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express, { Application } from 'express';
-import { Server } from 'http';
+import cors from 'cors';
 
 export class SetupServer extends Application {
   private server: http.Server;
@@ -7,4 +7,27 @@ export class SetupServer extends Application {
   constructor(private port = process.env.APP_PORT || 3333) {
     super();
   }
+
+  public async init(): Promise<void> {
+    this.setupExpress();
+  }
+
+  private getApp(): Application {
+    return this.app;
+  }
+
+  private setupExpress(): void {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(cors({ origin: '*' }));
+  }
+
+  public listen(): void {
+    this.server = this.app.listen(this.port, () => {
+      console.log(`Server is running on port ${this.port}`);
+    });
+  }
 }
+
+// add compression
+// add helmet
